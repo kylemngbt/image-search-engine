90c6d32585e82b3fc328a7cd1b51d4f9b7b8f2f4
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-10-04 21:52:02 +0800

    build: define accessKey

--- javascript/script.js
+++ javascript/script.js
@@ -2,8 +2,7 @@ const searchForm = document.querySelector("#search-form");
 const searchBox = document.querySelector("#search-box");
 const searchResult = document.querySelector("#search-result");
 const showMoreBtn = document.querySelector("#show-more-btn");
-
+const accessKey = 'NZDu_zPf5vI9vpifUsMbshvpkfyGPbOstoLTr9S-ln0';
 
 let keyword = "";
 let page = 1;
-
