1f541fe85fd88f3ea4b5c3b2a165f9ee515a743f
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-10-04 21:49:22 +0800

    build: define two vars

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,8 @@ const searchForm = document.querySelector("#search-form");
 const searchBox = document.querySelector("#search-box");
 const searchResult = document.querySelector("#search-result");
 const showMoreBtn = document.querySelector("#show-more-btn");
+
+
+let keyword = "";
+let page = 1;
+
