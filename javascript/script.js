2a0fa334e08d340743e522bad9f85ebe0d69ada7
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-10-04 21:59:45 +0800

    feat: define the initial searchImages async func

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,25 @@ const accessKey = 'NZDu_zPf5vI9vpifUsMbshvpkfyGPbOstoLTr9S-ln0';
 
 let keyword = "";
 let page = 1;
+
+async function searchImages() {
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+  console.log(url);
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  })
+}
